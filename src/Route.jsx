5902ed775302e8d9e08573a619bad5c6e61db524
Precise Goals <elitesarthakamr@gmail.com>
2025-12-02 05:44:14 +0530

    module roll up error

--- src/Route.jsx
+++ src/Route.jsx
@@ -9,6 +9,9 @@ import { LEducationLoan } from "./components/LEducationLoan";
 import { Hero } from "./components/Hero";
 import { Services } from "./components/Services";
 import { MainServices } from "./components/MainServices";
+import { TrustedPartner } from "./components/TrustedPartner";
+import { AddCareer } from "./components/addCareer";
+import { Careers } from "./components/Careers";
 
 export const Dat = [
   {
@@ -60,5 +63,20 @@ export const Dat = [
     path: "/machinery-loan",
     label: "Machinery Loan",
     element: <LMachineryLoan />,
+  },
+  {
+    path: "/trusted-partner",
+    label: "Trusted Partner",
+    element: <TrustedPartner />,
+  },
+  {
+    path: "/careers",
+    label: "Careers",
+    element: <Careers />,
+  },
+  {
+    path: "/admin",
+    label: "Add Career",
+    element: <AddCareer />,
   },
 ];
