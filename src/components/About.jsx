87644f0a0f569950c886863dcb97b57132d713f2
Precise Goals <elitesarthakamr@gmail.com>
2026-03-10 20:56:23 +0530

    okay bhai

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -166,17 +166,33 @@ export const About = () => {
           {[
             1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
             20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
-            37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52
+            37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+            54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+            71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
           ]
-            .slice(0, expandPartners ? 52 : 12)
-            .map((num) => (
-              <img
-                key={num}
-                src={`/part/${num}.png`}
-                alt={`Partner ${num}`}
-                className="ab-partner-logo desai"
-              />
-            ))}
+            .slice(0, expandPartners ? 84 : 18)
+            .map((num) => {
+              const extGroups = {
+                svg: [7, 14, 17, 18, 22, 83, 84],
+                webp: [
+                  8, 9, 10, 11, 12, 13, 15, 16, 19, 20, 21, 23, 74, 75, 76, 77,
+                  78, 79, 80, 81, 82,
+                ],
+                jpg: [53],
+              };
+              const ext =
+                Object.keys(extGroups).find((key) =>
+                  extGroups[key].includes(num),
+                ) || "png";
+              return (
+                <img
+                  key={num}
+                  src={`/part/${num}.${ext}`}
+                  alt={`Partner ${num}`}
+                  className="ab-partner-logo desai"
+                />
+              );
+            })}
         </div>
 
         <button
